7a158060392475a3ac34df56d4cb6c5475b5f824
luyanbo <robert.lyb@alibaba-inc.com>
2021-10-26 14:27:41 +0800

    NewFIFOSetInDir

--- cio/io_unix.go
+++ cio/io_unix.go
@@ -56,9 +56,6 @@ func NewFIFOSetInDir(root, id string, terminal bool) (*FIFOSet, error) {
 	// 	}
 	// }
 	dir := filepath.Join(root, randStringRunes(8))
-	if err := os.MkdirAll(dir, 0700); err != nil {
-		return nil, errors.WithStack(err)
-	}
 	closer := func() error {
 		return errors.New("unimplemented")
 	}
